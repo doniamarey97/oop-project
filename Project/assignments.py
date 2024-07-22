ffbabac1e1108c7845ffb5982b6e3b8a1a9a6c37
Donia Marey <166507148+doniamarey97@users.noreply.github.com>
2024-07-22 19:51:08 +0200

    Update assignments.py

--- Project/assignments.py
+++ Project/assignments.py
@@ -1,8 +1,19 @@
 class Assignment:
-    assignment_info={
-        "course_code":["CS111","CS112","CS123","CS333","CS136","CS240","CS350"],
-        "assignment":["task111","task112","task123","task333","task136","task240","task350"]
-    }
+    def __init__(self, name, course_code):
+        self.name = name
+        self.course_code = course_code
+        self.solutions = {}
+        self.grades = {}
 
+    def submit_solution(self, student_id, solution_text):
+        self.solutions[student_id] = solution_text
+        print(f"Solution submitted for student {student_id}: {solution_text}")
+
+    def set_grade(self, student_id, grade):
+        self.grades[student_id] = grade
+
+    def view_grades_report(self):
+        for student_id, grade in self.grades.items():
+            print(f"Student ID: {student_id}, Grade: {grade}")
        
 
